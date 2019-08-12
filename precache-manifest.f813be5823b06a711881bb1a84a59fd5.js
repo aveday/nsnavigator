0e9e4992f1c77655a47cd432e0eff98c0b58d7d8
David Williams <david@aveday.net>
2019-08-12 18:57:31 +0930

    Updates

---
+++ precache-manifest.f813be5823b06a711881bb1a84a59fd5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ce5c43d66c542df617d358cff3786a9",
+    "url": "/nsnavigator/index.html"
+  },
+  {
+    "revision": "3a943a28a7bdaa9eb35b",
+    "url": "/nsnavigator/static/css/main.c55e54c5.chunk.css"
+  },
+  {
+    "revision": "151c780d8b0a73f95ff3",
+    "url": "/nsnavigator/static/js/2.fd41f47a.chunk.js"
+  },
+  {
+    "revision": "3a943a28a7bdaa9eb35b",
+    "url": "/nsnavigator/static/js/main.de5c60a2.chunk.js"
+  },
+  {
+    "revision": "a93eb66547b8d4047faa",
+    "url": "/nsnavigator/static/js/runtime~main.94064e2a.js"
+  },
+  {
+    "revision": "28a8745cb6ffc5e1127a809fa113f445",
+    "url": "/nsnavigator/static/media/mike.28a8745c.mp3"
+  },
+  {
+    "revision": "e6c3b6393cfc07e56a64e0d5afbd1f81",
+    "url": "/nsnavigator/static/media/president.e6c3b639.mp3"
+  },
+  {
+    "revision": "ea5ea5657e0c9c27428cccba64082deb",
+    "url": "/nsnavigator/static/media/shutup.ea5ea565.mp3"
+  },
+  {
+    "revision": "70ceb45ee7a59cf39a59fc432fa77fb4",
+    "url": "/nsnavigator/static/media/welcome.70ceb45e.mp3"
+  }
+]);
