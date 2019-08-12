fe3d2ff3ecd6d487f57787d32f00dc47e6b53bba
David Williams <david@aveday.net>
2019-08-12 21:10:40 +0930

    Rearrange layout

--- src/audio.js
+++ src/audio.js
@@ -30,7 +30,6 @@ const playClip = clip => {
   audioElement.src = clip
   audioElement.play()
 
-  const section = document.querySelector('#audio')
   const canvas = document.querySelector('canvas')
   const ctx = canvas.getContext("2d")
   scope.stop(ctx)
@@ -40,6 +39,9 @@ const playClip = clip => {
 class AudioComponent extends React.Component {
   componentDidMount() {
     const canvas = document.querySelector('canvas')
+    canvas.width = document.body.clientWidth / 3;
+    canvas.height = document.body.clientHeight / 3;
+
     const ctx = canvas.getContext("2d")
     ctx.strokeStyle = 'red'
     ctx.lineWidth = '2'
@@ -55,24 +57,22 @@ class AudioComponent extends React.Component {
   render() {
     return (
       <section id='audio'>
-        <canvas >
+        <canvas>
         </canvas>
 
         <div className='buttons'>
           <div className='row'>
             <div className='button' onClick={()=>playClip(clip1)}>NAV</div>
             <div className='button' onClick={()=>playClip(clip2)}>INT</div>
+            <div className='button' onClick={()=>playClip(clip1)}>REG</div>
           </div>
 
           <div className='row'>
             <div className='button' onClick={()=>playClip(clip3)}>LOG</div>
             <div className='button' onClick={()=>playClip(clip4)}>RND</div>
-          </div>
-
-          <div className='row'>
-            <div className='button' onClick={()=>playClip(clip1)}>REG</div>
             <div className='button' onClick={()=>playClip(clip1)}>POW</div>
           </div>
+
         </div>
       </section>
     )
